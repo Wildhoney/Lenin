78d23732cc7b10b6dbe102dee5aa2b9a99412015
Wildhoney <adam.timberlake@gmail.com>
2015-09-05 23:48:59 +0100

    Added constants and strategies

---
+++ src/shapes/Constants.js
@@ -0,0 +1,11 @@
+/**
+ * @constant LN_POS_RELATIVE
+ * @type {String}
+ */
+export const LN_POS_RELATIVE = 'relative';
+
+/**
+ * @constant LN_POS_ABSOLUTE
+ * @type {String}
+ */
+export const LN_POS_ABSOLUTE = 'absolute';
--- src/shapes/utilities/Methods.js
+++ src/shapes/utilities/Methods.js
@@ -1,3 +1,11 @@
+import * as c from './../Constants';
+
+/**
+ * @constant DEFAULT_POSITION_STRATEGY
+ * @type {String}
+ */
+const DEFAULT_POSITION_STRATEGY = c.LN_POS_ABSOLUTE;
+
 /**
  * @module Lenin
  * @submodule Methods
@@ -14,6 +22,28 @@ export default function methods({ shape, group, collection }) {
          */
         remove: function() {
             group.remove();
+        },
+
+        /**
+         * @method position
+         * @param {Number} [x]
+         * @param {Number} [y]
+         * @param {String} [strategy=DEFAULT_POSITION_STRATEGY]
+         * @return {Object|void}
+         */
+        position: function position({ x, y }, strategy = DEFAULT_POSITION_STRATEGY) {
+
+        },
+
+        /**
+         * @method dimensions
+         * @param {Number} [width]
+         * @param {Number} [height]
+         * @param {String} [strategy=DEFAULT_POSITION_STRATEGY]
+         * @return {Object|void}
+         */
+        dimensions: function position({ width, height }, strategy = DEFAULT_POSITION_STRATEGY) {
+
         }
 
     }
